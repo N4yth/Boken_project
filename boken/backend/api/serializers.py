2f28bdf9c94c689d7088607a4febf645bc90aa69
N4yth <nathan.dupuis31@gmail.com>
2025-10-04 15:41:56 +0200

    feat : add the hash password when user is create

--- boken/backend/api/serializers.py
+++ boken/backend/api/serializers.py
@@ -2,6 +2,14 @@ from rest_framework import serializers
 from .models.user import User
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'role']
+        fields = ['id', 'username', 'email', 'role', 'password']
+        read_only_fields = ['role']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User.objects.create_user(**validated_data, password=password)
+        return user
