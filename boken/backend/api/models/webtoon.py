43d92b9ba16030e7d60140fbbd4c9b3c5d4ccb45
N4yth <nathan.dupuis31@gmail.com>
2025-10-07 11:08:00 +0200

    fix : remove manager add foreign key to user and a variable

--- boken/backend/api/models/webtoon.py
+++ boken/backend/api/models/webtoon.py
@@ -1,29 +1,20 @@
 from django.db import models
 from .base_model import BaseModel
-
-
-class WebtoonManager(models.Manager):
-    def create_webtoon(self, title, authors, release_date, status, is_public, rating, add_by):
-        if not title:
-            raise ValueError("Le titre est obligatoire")
-        if not authors:
-            raise ValueError("Les auteurs sont obligatoires")
-        if not release_date:
-            raise ValueError("Le titre est obligatoire")
-        if not status:
-            raise ValueError("Les auteurs sont obligatoires")
-
-        webtoon = self.model(title=title, authors=authors, release_date=release_date, status=status, is_public=is_public, rating=rating, add_by=add_by)
-        webtoon.save(using=self._db)
-        return webtoon
+from .user import User
 
 
 class Webtoon(BaseModel):
     title = models.CharField(max_length=255, unique=True, null=False, blank=False)
     authors = models.CharField(max_length=255, null=False, blank=False)
-    release_date = models.DateTimeField(null=False, blank=False)
+    release_date = models.DateField(default='2000-01-01', null=False, blank=False)
     status = models.CharField(max_length=64, null=False, blank=False)
     is_public = models.BooleanField(default=False)
     rating = models.FloatField(default=0.0)
-    add_by = models.CharField(max_length=255, default='')
-    
+    add_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='webtoons'
+    )
+    waiting_review = models.BooleanField(default=False)
