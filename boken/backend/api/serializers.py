be5a5767a0575ec05662398b106579d23198de94
N4yth <nathan.dupuis31@gmail.com>
2025-10-07 11:05:49 +0200

    feat : setup new read only for user and create the webtoon serializer with field and read only data

--- boken/backend/api/serializers.py
+++ boken/backend/api/serializers.py
@@ -7,8 +7,8 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'role', 'password'] # add 'create_at', 'update_at'
-        read_only_fields = ['role']
+        fields = ['id', 'username', 'email', 'role', 'password', 'create_at', 'update_at']
+        read_only_fields = ['id', 'role', 'create_at', 'update_at']
 
     def create(self, validated_data):
         password = validated_data.pop('password')
@@ -16,8 +16,9 @@ class UserSerializer(serializers.ModelSerializer):
         return user
 
 class WebtoonSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True)
+    add_by = UserSerializer(read_only=True)
 
     class Meta:
         model = Webtoon
-        fields = ['id', 'title', 'authors', 'release_date', 'title', 'status', 'is_public', 'rating', 'create_at', 'update_at']
+        fields = ['id', 'title', 'authors', 'status', 'is_public', 'rating', 'add_by', 'release_date', 'create_at', 'update_at', 'waiting_review']
+        read_only_fields = ['id', 'is_public', 'add_by', 'created_at', 'release_date', 'update_at'] 
