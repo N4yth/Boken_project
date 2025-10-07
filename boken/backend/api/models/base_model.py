70f39bbcca2dce1213761b6e81a56249714d7832
N4yth <nathan.dupuis31@gmail.com>
2025-10-07 11:06:38 +0200

    fix : set the type for 2 variable with more precise way and with module from django

--- boken/backend/api/models/base_model.py
+++ boken/backend/api/models/base_model.py
@@ -1,15 +1,15 @@
 from django.db import models
 import uuid
-from datetime import datetime
+from django import utils
 
 class BaseModel(models.Model):
     id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
-    create_at = models.DateField(default = datetime.now, editable = False)
-    update_at = models.DateField(default = datetime.now)
+    create_at = models.DateTimeField(default = utils.timezone.now, editable = False)
+    update_at = models.DateTimeField(default = utils.timezone.now)
 
     def SaveDate(self):
         """Update the updated_at timestamp whenever the object is modified"""
-        self.updated_at = datetime.now()
+        self.updated_at = utils.timezone.now()
 
     def update(self, data):
         """Update the attributes of the object based on the provided dictionary"""
