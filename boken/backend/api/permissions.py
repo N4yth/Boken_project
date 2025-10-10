a52bbc1048751fb549834cb8d7cbb370ae3ad98f
N4yth <nathan.dupuis31@gmail.com>
2025-10-10 10:10:23 +0200

    feat : add permission for webtoon creator or admin

--- boken/backend/api/permissions.py
+++ boken/backend/api/permissions.py
@@ -1,4 +1,5 @@
 from rest_framework.permissions import BasePermission
+from api.models.webtoon import Webtoon
 
 class IsSelfOrAdmin(BasePermission):
     def has_object_permission(self, request, view, obj):
@@ -11,3 +12,20 @@ class IsCreatorOrAdmin(BasePermission):
         if hasattr(request.user, "role") and request.user.role == "admin":
             return True
         return obj.add_by.id == request.user.id
+
+class IsWebtoonCreatorOrAdmin(BasePermission):
+    def has_permission(self, request, view):
+        if request.user or request.user.is_authenticated:
+            if request.user.is_staff or getattr(request.user, "role", "") == "admin":
+                return True
+            webtoon_id = request.data.get("webtoon_id")
+            if not webtoon_id:
+                return False
+            try:
+                webtoon = Webtoon.objects.get(pk=webtoon_id)
+            except Webtoon.DoesNotExist:
+                return False
+            if hasattr(webtoon, "add_by") and webtoon.add_by.id == request.user.id:
+                return True
+            return False
+        return False
