9ce4d3388e501dac356355cbb89ebe346da1569d
N4yth <nathan.dupuis31@gmail.com>
2025-10-14 09:43:43 +0200

    feat : update DB with migration

--- boken/backend/api/migrations/0003_webtoon_waiting_review.py
+++ boken/backend/api/migrations/0003_webtoon_waiting_review.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0002_alter_user_create_at_alter_user_update_at_and_more'),
+        ('api', '0001_initial'),
     ]
 
     operations = [
