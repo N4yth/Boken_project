e1f96e7e5f4fafb01e58adfaa685b5efe624bd1e
N4yth <nathan.dupuis31@gmail.com>
2025-10-15 09:39:00 +0200

    feat : call the script to init 100 webtoon

--- boken/backend/api/views/admin_command.py
+++ boken/backend/api/views/admin_command.py
@@ -2,6 +2,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAdminUser
+from api.update_from_anilist import update_all_from_anilist
 
 @api_view(['GET'])
 @permission_classes([IsAdminUser])
@@ -18,5 +19,5 @@ def update(request):
 @api_view(['GET'])
 @permission_classes([IsAdminUser])
 def create_new(request):
-    print("test create_new")
+    update_all_from_anilist(max_count=100)
     return Response({"message": "Création réussie"}, status=status.HTTP_200_OK)
