7e99e96655bbfb44bec40edd4febe5bc34546095
N4yth <nathan.dupuis31@gmail.com>
2025-10-20 09:16:03 +0200

    feat : add admin route

--- boken/backend/backend/urls.py
+++ boken/backend/backend/urls.py
@@ -1,4 +1,3 @@
-from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from rest_framework_simplejwt.views import (
@@ -10,7 +9,7 @@ from api.views.webtoon import WebtoonViewSet
 from api.views.genre import GenreViewSet
 from api.views.release import ReleaseViewSet
 from api.views.user_release import UserReleaseViewSet
-from api.views.admin_command import update_all, create_new, update
+from api.views.admin_command import update_all, create_new, get_progress
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -22,7 +21,7 @@ router.register(r'usereleases', UserReleaseViewSet)
 urlpatterns = [
     path('admin/update_all/', update_all, name='update_all'),
     path('admin/create/', create_new, name='create_new'),
-    path('admin/update/', update, name='update'),
+    path('admin/update/', get_progress, name='update'),
 
     path('api/', include(router.urls)),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
