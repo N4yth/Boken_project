62d8cb4cb77acacf484347633468a8653648f1e3
N4yth <nathan.dupuis31@gmail.com>
2025-10-21 09:00:00 +0200

    fix: remove useless function to update the date of update for every element

--- boken/backend/api/models/base_model.py
+++ boken/backend/api/models/base_model.py
@@ -5,18 +5,7 @@ from django import utils
 class BaseModel(models.Model):
     id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
     create_at = models.DateTimeField(default = utils.timezone.now, editable = False)
-    update_at = models.DateTimeField(default = utils.timezone.now)
-
-    def SaveDate(self):
-        """Update the updated_at timestamp whenever the object is modified"""
-        self.updated_at = utils.timezone.now()
-
-    def update(self, data):
-        """Update the attributes of the object based on the provided dictionary"""
-        for key, value in data.items():
-            if hasattr(self, key):
-                setattr(self, key, value)
-        self.SaveDate()
+    update_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
